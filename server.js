429e0b90e2cb8e77796c76da082d3d95b805962a
Mayur <dev.mayurvishawakarma@gmail.com>
2024-06-07 12:12:51 +0530

    server changes 2024-06-07

--- server.js
+++ server.js
@@ -19,13 +19,13 @@ const requireToken = (req, res, next) => {
   }
 };
 
-// // Serve static files from the public folder
-// app.use(express.static('public'));
+// Serve static files from the public folder
+app.use(express.static('public'));
 
-// // Home page route
-// app.get('/', (req, res) => {
-//   res.sendFile(__dirname + '/public/Index/index.html');
-// });
+// Home page route
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/Index/index.html');
+});
 
 // Apply token middleware to all routes under /users
 app.use('/users', requireToken, userRoutes);
